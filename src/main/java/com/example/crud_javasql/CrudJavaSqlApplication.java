437cb28d5e5fa43430f0a35705caf04f68ef1df4
JMiguel <jmiguel.brandaosilva@gmail.com>
2026-04-20 11:57:38 -0300

    Commit 1

---
+++ src/main/java/com/example/crud_javasql/CrudJavaSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud_javasql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudJavaSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudJavaSqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crud_javasql/CrudJavaSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_javasql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJavaSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
